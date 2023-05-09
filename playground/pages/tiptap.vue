777044b29de2209dc5a484640d664eba16da8439
Benjamin VIELLARD <gbicou@gmail.com>
2023-05-09 16:29:13 +0200

    style: tiptap editor

--- playground/pages/tiptap.vue
+++ playground/pages/tiptap.vue
@@ -34,6 +34,60 @@ const editor = useEditor({
 .ProseMirror {
   min-height: 300px;
   overflow-y: auto;
+
+  > * + * {
+    margin-top: 0.75em;
+  }
+
+  ul,
+  ol {
+    padding: 0 1rem;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 1.1;
+  }
+
+  code {
+    background-color: rgba(#616161, 0.1);
+    color: #616161;
+  }
+
+  pre {
+    background: #0d0d0d;
+    color: #fff;
+    font-family: monospace;
+    padding: 0.75rem 1rem;
+    border-radius: 0.5rem;
+
+    code {
+      color: inherit;
+      padding: 0;
+      background: none;
+      font-size: 0.8rem;
+    }
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  blockquote {
+    padding-left: 1rem;
+    border-left: 2px solid rgba(#0d0d0d, 0.1);
+  }
+
+  hr {
+    border: none;
+    border-top: 2px solid rgba(#0d0d0d, 0.1);
+    margin: 2rem 0;
+  }
 }
 </style>
 
@@ -53,6 +107,7 @@ const editor = useEditor({
       }
     }
     &#editor {
+      padding: 0 $dt("space.8");
       min-width: 80ch;
     }
   }
